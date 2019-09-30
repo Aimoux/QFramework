844186a6729ab491265e498fcf33d16f3fcf4f13
Aimoux <oceanambition@163.com>
2019-09-30 17:10:09 +0800

    Q Res Loader
    Q Res Loader

--- Assets/Scripts/TimeLineManager.cs
+++ Assets/Scripts/TimeLineManager.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
 using UnityEngine.Playables;
+using QF.Res;
 
 public class TimeLineManager : MonoBehaviour
 {
     public static TimeLineManager Instance;
+    private ResLoader loader;
     private PlayableDirector director;
+
+    public Animator playerAnim;
+    public Animator botAnim;
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        loader.Recycle2Cache();
+        loader = null;
     }
 
     void Start()
@@ -19,18 +30,34 @@ public class TimeLineManager : MonoBehaviour
         director = GetComponent<PlayableDirector>();
         if (director == null)
             Debug.LogError("find no director: " + this.name);
+
+        loader = ResLoader.Allocate();
+        LaunchDirector("Resources/TimeLines/Meet");
     }
     
     public void LaunchDirector(string path)
     {
-        director.playableAsset = Resources.Load<PlayableAsset>("TimeLines/Meet");
-        //director.SetGenericBinding(null,null);//??
-        director.Play();
+        Debug.Log("Binding Timeline Tracks!");
+        //director.playableAsset = Resources.Load<PlayableAsset>(path);
+        director.playableAsset = loader.LoadSync<PlayableAsset>(path);//“Ï≤Ωº”‘ÿ¥Ê‘⁄“˛ªº??        
+        foreach (var playableAssetOutput in director.playableAsset.outputs)
+        {
+            if (playableAssetOutput.streamName == "PlayerAnim")
+            {
+                director.SetGenericBinding(playableAssetOutput.sourceObject, playerAnim);
+            }
+            else if (playableAssetOutput.streamName == "BotAnim")
+            {
+                director.SetGenericBinding(playableAssetOutput.sourceObject, botAnim);
+            }
+        }
+        director.Play();       
     }
 
     public void Stop()
     {
         director.Stop();
+        loader.Recycle2Cache();
     }
 
 }
